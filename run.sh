01f7052a8c104ed7b27d0a9eb91c41eaccae31e4
Benedikt Kristinsson <benedikt@lokun.is>
2021-01-27 18:47:31 +0100

    separate audo and video messages

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ docker build -t benediktkr/sudoisytdl:latest .
 env_file="./dev.env"
 source ${env_file}
 
-docker run -v ${WEB_DIR_HOST}:${WEB_DIR} -v ${DL_DIR_HOST}:${DL_DIR} --env-file ${env_file} --name sudoisytdl --rm -it benediktkr/sudoisytdl:latest $@
+docker run -v ${WEB_DIR_HOST}:${WEB_DIR} -v ${DL_DIR_HOST}:${DL_DIR} --env-file ${env_file} --name sudoisytdl --rm -it benediktkr/sudoisytdl:latest run ytdl $@
